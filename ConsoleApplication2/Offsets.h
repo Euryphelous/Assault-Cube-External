26118467f05a10adff27387db9878d7c7d307975
Euryphelous <42641981+Euryphelous@users.noreply.github.com>
2018-08-23 20:44:35 +0300

    Add files via upload

---
+++ ConsoleApplication2/Offsets.h
@@ -0,0 +1,46 @@
+#ifndef Offsets
+#define Offsets
+DWORD assault_cube_base;
+DWORD LOCAL_PLAYER = 0x509B74;
+DWORD NAME = 0x225;
+DWORD HP = 0xF8;
+DWORD AP = 0xFC;
+DWORD X = 0x34;
+DWORD Y = 0x38;
+DWORD Z = 0x3C;
+DWORD Grenade = 0x158;
+DWORD CM = 0x1E4;
+/////////////////////////////
+DWORD procID;
+/////////////////////////////
+bool space = false;
+bool space_2 = false;
+bool space_3 = false;
+bool hackEnabled = false;
+/////////////////////////////
+int lAdress;
+int baseAdress;
+int firstAdress;
+int secondAdress;
+int newBase;
+char sName[20];
+int sCM;
+int sHP;
+int sAP;
+float sX;
+float sY;
+float sZ;
+int TeleportX;
+int TeleportY;
+int TeleportZ;
+int sGrenade;
+int newHP;
+int newAP;
+int newGrenade;
+int newSmallPlayer;
+float newTeleportX;
+float newTeleportY;
+float newTeleportZ;
+////////////////////////////
+#endif // !Offsets
+
